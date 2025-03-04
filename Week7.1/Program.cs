dda68681b6839e16cb9325059fd85e6a1573cdf7
mlmoller21@amarillocollege.com <mlmoller21@amarillocollege.com>
2025-03-03 21:46:59 -0600

    Add project files.

---
+++ Week7.1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+
+    // Constructor
+    public Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+}
+
+public class JsonSerializerHelper
+{
+    // Method to serialize a Book object to JSON and save it in a file
+    public static void SerializeToJson(string fileName, Book book)
+    {
+        try
+        {
+            //Serialize the book object to a JSON string
+            string json = JsonConvert.SerializeObject(book);
+
+            // Write the JSON string to the specified file
+            File.WriteAllText(fileName, json);
+            Console.WriteLine($"Book details serialized and saved to {fileName}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while serializing the book: {ex.Message}");
+        }
+    }
+
+    // Method to deserialize the JSON file back into a Book object
+    public static Book DeserializeFromJson(string fileName)
+    {
+        try
+        {
+            // Read the JSON string from the file
+            string json = File.ReadAllText(fileName);
+
+            // Deserialize the JSON string back into a Book object
+            Book book = JsonConvert.DeserializeObject<Book>(json);
+            Console.WriteLine("Book details deserialized successfully.");
+            return book;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while deserializing the book: {ex.Message}");
+            return null;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create a book object
+        Book myBook = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+
+        // Specify the file name to save the serialized book
+        string fileName = "book.json";
+
+        // Serialize the book object to a JSON file
+        JsonSerializerHelper.SerializeToJson(fileName, myBook);
+
+        // Deserialize the book object from the JSON file
+        Book deserializedBook = JsonSerializerHelper.DeserializeFromJson(fileName);
+
+        // Display the deserialized book details
+        if (deserializedBook != null)
+        {
+            Console.WriteLine($"Book Title: {deserializedBook.Title}");
+            Console.WriteLine($"Author: {deserializedBook.Author}");
+            Console.WriteLine($"Year: {deserializedBook.Year}");
+        }
+    }
+}
---
+++ Week7.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Week7.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week7.1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00d17eec-05a6-4c87-a232-45990e85a99d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
